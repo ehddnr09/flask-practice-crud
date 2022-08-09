477ffd2433d14e3c5fea7bde57ca146bc7d59f3d
ehddnr09 <ilovpark@naver.com>
2022-08-09 23:58:38 +0900

    db_init 생성

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
-import os  # 경로설정을 위한 패키지
-from flask_pymongo import PyMongo
-from flask.json import jsonify
+import os
 from flask import Flask, render_template, request, redirect
+from db_init import db
 
 app = Flask(__name__, static_url_path='/static')
-app.config['MONGO_URI'] = "mongodb://localhost:27017/local"
-mongo = PyMongo(app)
-
-
-@app.route('/detail')
-def detail():
-    market_db = mongo.db.market
-    market = market_db.find_one({"title": request.args.get('title')})
-
-    return render_template('detail.html', market=market)
 
 
 @app.route('/delete', methods=["post"])
 def delete():
-    market_db = mongo.db.market
+    market_db = db['market']
     title = request.form.get('title')
     market = market_db.find_one({"title": title})
 
@@ -26,9 +15,12 @@ def delete():
     return redirect('/')
 
 
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
+@app.route('/detail')
+def detail():
+    market_db = db['market']
+    market = market_db.find_one({"title": request.args.get('title')})
+
+    return render_template('detail.html', market=market)
 
 
 @app.route('/write', methods=["post"])
@@ -39,7 +31,7 @@ def write():
     filepath = os.path.join(filepath, 'static')
     fileinfo.save(os.path.join(filepath, fileinfo.filename))
 
-    market_db = mongo.db.market
+    market_db = db['market']
 
     market_db.insert_one({
         "title": request.form.get('title'),
@@ -52,9 +44,14 @@ def write():
     return redirect('/')
 
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
 @app.route('/')
 def index():
-    market_db = mongo.db.market
+    market_db = db['market']
     markets = market_db.find()
     return render_template('index.html', markets=markets)
 
